ef2e028ad21f3290e9188fa8eb57184a16770a40
helinav <helina.valb@gmail.com>
2023-09-14 11:20:52 +0300

    AvatarView updated, working on PlayView

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -9,23 +9,26 @@
     <form>
       <div class="mb-3">
         <label for="exampleInputUsername" class="form-label">Kasutajanimi</label>
-        <input v-model="this.userInfo.username" type="text" class="form-control" placeholder="Kasutajanimi" aria-label="Username"
+        <input v-model="this.userInfo.username" type="text" class="form-control" placeholder="Kasutajanimi"
+               aria-label="Username"
                aria-describedby="basic-addon1">
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Parool</label>
-        <input v-model="this.userInfo.password" type="password" class="form-control" placeholder="Kirjuta siia tugev salasõna"
+        <input v-model="this.userInfo.password" type="password" class="form-control"
+               placeholder="Kirjuta siia tugev salasõna"
                id="exampleInputPassword1">
       </div>
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">E-maili address</label>
-        <input v-model="this.userInfo.email" type="email" class="form-control" placeholder="example@example.com" id="exampleInputEmail1"
+        <input v-model="this.userInfo.email" type="email" class="form-control" placeholder="example@example.com"
+               id="exampleInputEmail1"
                aria-describedby="emailHelp">
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
       </div>
     </form>
     <div class="mb-3">
-
+      <AvatarImage :image-data-base64="imageDataFromDatabase"/>
     </div>
     <button @click="" type="submit" class="btn btn-outline-success">Loo uus kasutaja</button>
     <AlertSuccess :alert-message="successMessage"/>
@@ -34,17 +37,24 @@
 </template>
 
 <script>
-
 import router from "@/router";
 import AlertSuccess from "@/components/alert/AlertSuccess.vue";
 import AlertDanger from "@/components/alert/AlertDanger.vue";
 import {EMAIL_ALREADY_EXISTS, USERNAME_ALREADY_EXISTS} from "@/assets/script/ErrorCode";
 import {ACCOUNT_ADDED, FILL_MANDATORY_FIELDS} from "@/assets/script/AlertMessage";
+import AvatarImage from "@/components/AvatarImage.vue";
 
 export default {
-  components: {AlertDanger, AlertSuccess},
+  components: {AvatarImage, AlertDanger, AlertSuccess},
   data() {
     return {
+      avatars: [
+        {
+          imageId: 0,
+          imageData: ''
+        }
+      ],
+      imageDataFromDatabase: '',
       userInfo: {
         username: '',
         password: '',
@@ -55,15 +65,9 @@ export default {
       errorResponse: {
         message: '',
         errorCode: 0
-      }
+      },
     }
   },
-
-  methods: {
-
-
-  }
-
 }
 
 </script>
