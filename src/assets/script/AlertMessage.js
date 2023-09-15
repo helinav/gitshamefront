9461fd0bee3abd412cf716d537e5c78da018ff89
helinav <helina.valb@gmail.com>
2023-09-15 10:04:16 +0300

    AlertMessage, errorCode added. gameview - possible to add game

--- src/assets/script/AlertMessage.js
+++ src/assets/script/AlertMessage.js
@@ -1,3 +1,4 @@
 export const FILL_MANDATORY_FIELDS = 'Täida kõik väljad'
 export const ACCOUNT_ADDED = 'Kasutaja on registeeritud'
 export const AVATAR_ADDED = "Avatar on süsteemi lisatud"
+export const GAME_NAME_ADDED = "Uue mängu nimi on süsteemi lisatud"
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,17 +1,69 @@
 <template>
   <div class="text-center">
+    <AlertSuccess :alert-message="successMessage"/>
+    <AlertDanger :alert-message="errorMessage"/>
     <h1>Lisa uus mäng</h1>
   </div>
   <div class="input-group justify-content-center mb-3">
-    <input type="text" class="form-control w-25" placeholder="Kirjuta mängu nimi" aria-label="Mängu nimi" aria-describedby="basic-addon1">
+    <input type="text" class="form-control w-25" placeholder="Kirjuta mängu nimi" aria-label="Mängu nimi"
+           aria-describedby="basic-addon1">
   </div>
-  <div @click="">
+  <div @click="addGame">
     <button class="btn btn-outline-success">Lisa mäng</button>
   </div>
 </template>
 
 <script>
+import AlertSuccess from "@/components/alert/AlertSuccess.vue";
+import {GAME_NAME_ADDED} from "@/assets/script/AlertMessage";
+import AlertDanger from "@/components/alert/AlertDanger.vue";
+import {GAME_NAME_ALREADY_EXISTS} from "@/assets/script/ErrorCode";
+
 export default {
-  name: 'GameView'
+  name: 'GameView',
+  components: {AlertDanger, AlertSuccess},
+  data() {
+    return {
+      successMessage: '',
+      errorMessage: '',
+      gameId: 0,
+      gameName: '',
+    }
+  },
+  methods: {
+
+    addGame() {
+      this.sendGameRequest()
+    },
+
+
+    sendGameRequest() {
+      this.$http.post("/game", this.gameId, {
+            params: {
+              gameName: this.gameName,
+            }
+          }
+      ).then(response => {
+        this.handleGameRequestSuccessResponse()
+      }).catch(error => {
+        this.handleGameRequestErrorResponse()
+      })
+    },
+
+    handleGameRequestSuccessResponse() {
+      this.successMessage = GAME_NAME_ADDED
+      this.resetField()
+    },
+
+    handleGameRequestErrorResponse() {
+      this.errorMessage = GAME_NAME_ALREADY_EXISTS
+    },
+
+    resetField() {
+      this.gameName = ''
+    },
+
+  }
+
 }
 </script>
