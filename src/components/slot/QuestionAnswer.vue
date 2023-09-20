a66e5d5527adf068240275baa945fa9ce420aec9
helinav <helina.valb@gmail.com>
2023-09-20 10:12:58 +0300

    QuestionAnswer wip

--- src/components/slot/QuestionAnswer.vue
+++ src/components/slot/QuestionAnswer.vue
@@ -2,9 +2,9 @@
   <div class="row justify-content-center mt-5">
     <div class=" col col-3">
       <div class="form-check">
-        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-        <label class="form-check-label" for="flexRadioDefault1">
-          Default radio
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+        <label class="form-check-label" for="flexCheckDefault">
+          Default checkbox
         </label>
       </div>
     </div>
@@ -12,6 +12,73 @@
 </template>
 <script>
 export default {
-  name: 'QuestionAnswer'
+  name: 'QuestionAnswer',
+  data() {
+    return {
+      questionId: 0,
+      selectResponse: [
+        {
+          answerId: 0,
+          text: '',
+          isSelected: true
+        }
+      ],
+      textBoxResponse: {
+        answerId: 0,
+        text: ''
+      },
+      sequenceResponse: [
+        {
+          answerId: 0,
+          text: '',
+          sequence: 0,
+          isSelected: true
+        }
+      ]
+    }
+  },
+
+  methods: {
+
+    newSelectableAnswer() {
+      this.$http.get("/answers/select", {
+            params: {
+              questionId: this.questionId
+            }
+          }
+      ).then(response => {
+        this.selectResponse = response.data
+      }).catch(error => {
+        const errorResponseBody = error.response.data
+      })
+    },
+
+    newTextBoxAnswer() {
+      this.$http.get("/answers/textbox", {
+            params: {
+              questionId: this.questionId
+            }
+          }
+      ).then(response => {
+        this.textBoxResponse = response.data
+      }).catch(error => {
+        const errorResponseBody = error.response.data
+      })
+    },
+
+    newSequenceAnswer() {
+      this.$http.get("/answers/sequence", {
+            params: {
+              questionId: this.questionId
+            }
+          }
+      ).then(response => {
+        this.sequenceResponse = response.data
+      }).catch(error => {
+        const errorResponseBody = error.response.data
+      })
+    },
+
+  }
 }
 </script>
